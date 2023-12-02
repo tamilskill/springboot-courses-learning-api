ec1e83393bcb278af526ebf4a9c5e4844e1684ae
Tamil Skillhub <tamilskillhub@gmail.com>
2023-12-02 13:30:39 +0530

    modified topic controller and add pojo class

--- src/main/java/com/springboot/topic/TopicController.java
+++ src/main/java/com/springboot/topic/TopicController.java
@@ -1,5 +1,8 @@
 package com.springboot.topic;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class TopicController {
 
 	@RequestMapping("/topics")
-	public String getAllTopics(){
-		return "All Topics";
+	public List<Topic> getAllTopics(){
+		return Arrays.asList(
+				new Topic("spring", "Spring Framework", "Spring Framework Description"),
+				new Topic("java", "Core Java", "Core Java Description"),
+				new Topic("javascript", "Javascript", "Javascript Framework Description")
+		);
 	}
 	
 }
