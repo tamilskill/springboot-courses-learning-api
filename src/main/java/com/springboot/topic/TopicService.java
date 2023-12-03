f3244744a305b861da0785e58e8404b1128c7ebd
Tamil Skillhub <tamilskillhub@gmail.com>
2023-12-03 16:16:25 +0530

    handle post request

--- src/main/java/com/springboot/topic/TopicService.java
+++ src/main/java/com/springboot/topic/TopicService.java
@@ -1,5 +1,6 @@
 package com.springboot.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,11 +9,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 	
-	public List<Topic> topics = Arrays.asList(
+	public List<Topic> topics = new ArrayList<>(Arrays.asList(
 				new Topic("spring", "Spring Framework", "Spring Framework Description"),
 				new Topic("java", "Core Java", "Core Java Description"),
 				new Topic("javascript", "Javascript", "Javascript Framework Description")
-		);
+		));
 
 	public List<Topic> getAllTopics() {
 		return topics;
@@ -21,6 +22,10 @@ public class TopicService {
 	public Topic getTopic(String id) {
 		// find topic and return it
 		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+	
+	public void addTopic(Topic topic) {
+		topics.add(topic);
 	}
 	
 }
